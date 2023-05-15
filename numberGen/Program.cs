1beefc94e63a2feae02cb72677d27203e42ab24a
Tom <tom.jackson442@hotmail.com>
2023-05-15 21:20:13 +0100

    Refactor code for number generation and logging
    This commit refactors the number generation and logging logic in the numberGen namespace. It improves readability and performance by introducing a more efficient way to increase the value range. Additionally, it fixes a bug in the console output where the range was not displayed correctly.
    
    Here are the changes made:
    
    Introduced a GenerateRandomNumber method to encapsulate the number generation logic.
    Simplified the loop condition by using true instead of the running variable.
    Renamed variables i and j to count and run respectively for better clarity.
    Modified the range calculation to use the val variable directly instead of using Math.Pow.
    Updated the console output to display the correct range value.
    These changes improve the efficiency and readability of the code.

---
+++ numberGen/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace numberGen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            bool running = true;
+            int i = 0;
+            int j = 1;
+            int val = 10;
+            int upperVal = (int)Math.Pow(10, 10);
+
+            while (running)
+            {
+                int number1 = random.Next(0, val);
+                int number2 = random.Next(0, val);
+                i += 1;
+                if (number1 == number2)
+                {
+                    Console.WriteLine("run: " + j + ", number: " + number1 + ", count: " + i + ", range: ", val.ToString());
+                    if (val != upperVal)
+                    {
+                        val *= 10;
+                        j += 1;
+                    }
+                    else
+                    {
+                        running = false;
+                    }
+
+                }
+            }
+
+
+        }
+    }
+}
